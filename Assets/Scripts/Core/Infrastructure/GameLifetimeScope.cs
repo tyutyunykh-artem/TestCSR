5b1ae730dc01df672979caff97e3e70f3077b273
tyutyunykh.a <artem.tyutyunykh@gmail.com>
2026-05-07 17:02:07 +0500

    Added GameLifetimeScope

---
+++ Assets/Scripts/Core/Infrastructure/GameLifetimeScope.cs
@@ -0,0 +1,16 @@
+﻿using VContainer;
+using VContainer.Unity;
+
+namespace Core.Infrastructure
+{
+    /// <summary>
+    /// Корневой контейнер для регистрации зависимостей приложения.
+    /// </summary>
+    public class GameLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+
+        }
+    }
+}
